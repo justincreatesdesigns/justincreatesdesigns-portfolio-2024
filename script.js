754485586fb8e8e8e766be7919318020acd7dc36
justincreatesdesigns <justin_jay_cabonce@yahoo.com>
2024-07-19 16:23:13 +0800

    added js to be able to update navbar

---
+++ script.js
@@ -0,0 +1,11 @@
+(async function () {
+  const headerHTMLRequest = await fetch("components/header.html");
+  const headerHTML = await headerHTMLRequest.text();
+  document.querySelector(".nav-start").innerHTML = headerHTML;
+})();
+
+(async function () {
+  const headerHTMLRequest = await fetch("components/footer.html");
+  const headerHTML = await headerHTMLRequest.text();
+  document.querySelector(".footer").innerHTML = headerHTML;
+})();

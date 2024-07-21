87e5e78687b29a18889f4b0d83fa223bec943296
justincreatesdesigns <justin_jay_cabonce@yahoo.com>
2024-07-21 18:27:38 +0800

    updated code

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 (async function () {
-  const headerHTMLRequest = await fetch("components/header.html");
+  const headerHTMLRequest = await fetch("/components/header.html");
   const headerHTML = await headerHTMLRequest.text();
   document.querySelector(".nav-start").innerHTML = headerHTML;
 })();
 
 (async function () {
-  const headerHTMLRequest = await fetch("components/all-works.html");
+  const headerHTMLRequest = await fetch("/components/all-works.html");
   const headerHTML = await headerHTMLRequest.text();
   document.querySelector(".all-works-js").innerHTML = headerHTML;
 })();
 
 (async function () {
-  const headerHTMLRequest = await fetch("components/footer.html");
+  const headerHTMLRequest = await fetch("/components/footer.html");
   const headerHTML = await headerHTMLRequest.text();
   document.querySelector(".footer").innerHTML = headerHTML;
 })();
